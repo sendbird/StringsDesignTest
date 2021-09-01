936379626049f865eaee510987f45d5e8fa0c315
Tez <tez.park@sendbird.com>
2021-09-02 01:54:18 +0900

    Added strings for test

---
+++ StringsDesignSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StringsDesignSample
+//
+//  Created by Tez Park on 2021/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
